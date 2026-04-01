cf8fc3b0ad38297c1189ff29790638c0e643af25
caio.devreee <caio.devreee@student.odisee.be>
2026-04-01 10:28:51 +0200

    stomme shi gefiskt123t

--- front/src/workers/quiz-worker.js
+++ front/src/workers/quiz-worker.js
@@ -2,11 +2,10 @@
 de gebruiker in de quiz, deze worker wordt gebruikt om de quiz resultaten te berekenen zonder 
 de hoofdthread te blokkeren, zodat de gebruikersinterface soepel blijft tijdens het berekenen 
 van de resultaten */
-
 import { getQuizResults } from '../lib/quiz-resultaten.js'
 
-self.onmessage = (event) => {
+globalThis.onmessage = (event) => {
   const { items, answers } = event.data
   const results = getQuizResults(items, answers)
-  self.postMessage(results)
+  globalThis.postMessage(results)
 }
